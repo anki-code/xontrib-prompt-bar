e10d621bdd61a9285672a7ebe226c594bd04ec23
Andy Kipp <anki-code@users.noreply.github.com>
2021-03-13 03:05:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setup(
     name='xontrib-prompt-bar',
-    version='0.2.6',
+    version='0.2.7',
     license='BSD',
     author='anki',
     author_email='author@example.com',
